0dac806b25a2dc86d1d1769d1077139ba0e034fd
Pedro Garcia <pedro@simbiotica.es>
2014-10-24 18:06:28 +0200

    pie nice

--- src/constructors/pie.js
+++ src/constructors/pie.js
@@ -46,33 +46,114 @@ Charicharts.Pie.prototype.init = function() {
     .value(function(d) {return d.value;});
 
   // Pie arc
-  var innerPadding = opts.outerBorder ? opts.outerBorder : 1;
+  var innerPadding = opts.outerBorder ? (1 - opts.outerBorder) : 1;
+  var arcRadius = $$.radius * innerPadding;
+
   $$.arc = d3.svg.arc()
-    .innerRadius(($$.radius * innerPadding) - (($$.radius * innerPadding) * (opts.innerRadius)))
-    .outerRadius($$.radius * innerPadding);
+    .innerRadius(arcRadius - (arcRadius * (1 - opts.innerRadius)))
+    .outerRadius(arcRadius); 
 
   // Draw pie
-  $$.svg.selectAll('path')
+  $$.pieces = $$.svg.selectAll('path')
       .data($$.pieLayout(opts.data))
       .enter()
     .append('path')
-    .attr('class', 'line')
+    .attr('class', 'pie-piece')
     .attr('fill', _.bind(function(d) {
       return d.data.color;
     }, this))
-    .on('mouseover', function(d) {
-      $$.svg.selectAll('path')
-        .style('opacity', opts.fadeOpacity);
-
-      d3.select(this).style('opacity', 1);
-      $$.trigger('mouseover', [d]);
-    })
     .attr('d', $$.arc);
 
-  // Inner arrow
-  if (opts.innerArrow.enabled) {
-      
+  // Mouse over event
+  $$.pieces.on('mouseover', function(d) {
+    // Fade all paths
+    $$.pieces
+      .style('opacity', opts.fadeOpacity);
+    // Highlight hovered
+    d3.select(this).style('opacity', 1);
+    // Triger over event
+    $$.trigger('mouseover', [d]);
+  });
+  
+  // Mouse leave event
+  $$.svg.on('mouseleave', function(d) {
+    $$.pieces
+      .style('opacity', 1);
+  });
+
+  if (opts.innerArrow) {
+    this.setInnerArrow();
   }
+};
+
+Charicharts.Pie.prototype.setInnerArrow = function() {
+  var opts = this._options;
+  var $$ = this.$scope;
+  var self = this;
+
+  var radius = $$.radius * (1 - opts.outerBorder);
+  var arrowSize = (radius * opts.innerArrowSize * (1 - opts.innerRadius));
+  var diameter = radius * (opts.innerRadius) * 2;
+
+  if (diameter < arrowSize) {
+    arrowSize = diameter * 0.5;
+  }
+
+  // Define arrow
+  $$.svg.append('svg:marker')
+      .attr('id', 'innerArrow')
+      .attr('viewBox', '0 {0} {1} {2}'.format(
+        -(arrowSize/2), arrowSize, arrowSize))
+      .attr('refX', (radius * (1-opts.innerRadius)) + 5)
+      .attr('refY', 0)
+      .attr('fill', 'white')
+      .attr('markerWidth', arrowSize)
+      .attr('markerHeight', arrowSize)
+      .attr('orient', 'auto')
+    .append('svg:path')
+      .attr('d', 'M0,{0}L{1},0L0,{2}'.format(
+        -(arrowSize/2), arrowSize, arrowSize/2));
+
+  // Draw arrow
+  $$.innerArrow = $$.svg.append('line')
+    .attr('class', 'outer-border')
+    .style('stroke', 'transparent')
+    .attr('x1', 0)
+    .attr('x2', 0)
+    .attr('y1', 0)
+    .attr('y2', 0)
+    .attr('marker-end', 'url(#innerArrow)');
+
+  // Set mouse move Event
+  $$.pieces.on('mousemove', function() {
+    var mouse = d3.mouse(this);
+    var angle = h_getAngle(mouse[0], mouse[1]);
+    moveArrow(angle);
+  });
+
+  function moveArrow(angle) {
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+    var x = radius * cos;
+    var y = radius * sin;
+    if (!x || !y) {return;}
+
+    $$.innerArrow
+      .attr('x2', x)
+      .attr('y2', y);
+  }
+
+  function triggerSelect(selection) {
+    selection.each(function(d) {
+      $$.trigger('mouseover', [d]);
+    });
+    var centroid = h_getCentroid(selection);
+    moveArrow(h_getAngle.apply(this, centroid));
+  }
+
+  setTimeout(function() {
+    triggerSelect(d3.select($$.pieces[0][0]));
+  }, 0);
 };
 
 /**
@@ -81,12 +162,9 @@ Charicharts.Pie.prototype.init = function() {
  */
 Charicharts.Pie.defaults = {
   margin: '0,0,0,0',
-  innerRadius: 0.22,
-  outerBorder: 0.9,
+  innerRadius: 0.5,
+  outerBorder: 0.1,
   fadeOpacity: 0.2,
-  innerArrow: {
-    enabled: false,
-    size: 10,
-    on: 'mouseover'
-  }
+  innerArrow: true,
+  innerArrowSize: 0.6
 };
--- src/core/helpers.js
+++ src/core/helpers.js
@@ -25,3 +25,44 @@ function h_parseOptions(opts) {
 
   return opts;
 }
+
+function h_getCentroid(selection) {
+  // get the DOM element from a D3 selection
+  // you could also use "this" inside .each()
+  var element = selection.node(),
+      // use the native SVG interface to get the bounding box
+      bbox = element.getBBox();
+  var centroid = [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
+  // return the center of the bounding box
+  return centroid;
+}
+
+function h_getAngle(x, y) {
+  var angle, referenceAngle;
+  if (x === 0 || y === 0) {return;}
+  referenceAngle = Math.atan(y/x);
+  referenceAngle += (referenceAngle < 0) ? Math.PI/2 : 0;
+
+  if (x >= 0 && y >= 0) {
+    angle = referenceAngle;
+  } else if (x <= 0 && y >= 0) {
+     angle = referenceAngle + (Math.PI/2);
+  } else if (x <= 0 && y <= 0) {
+    angle = referenceAngle + Math.PI;
+  } else if (x >= 0 && y <= 0) {
+    angle = referenceAngle + 3*(Math.PI/2);
+  } else {
+    return;
+  }
+
+  return angle;
+}
+
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] !== 'undefined' ? args[number] : match;
+    });
+  };
+}
