009c0ee55b77f6d10a475db05008ba76e837ca8f
Nacho Garcia <nachoab@gmail.com>
2014-12-16 03:34:33 +0100

    fix

--- demo/charicharts.js
+++ demo/charicharts.js
@@ -1108,13 +1108,12 @@ var p_series = PClass.extend({
     } else {
       var xStack = {};
       _.each(serie.data, function(serie) {
-        var stackSize = serie.values.length * barWidth;
-        console.log(stackSize);
+        // var stackSize = serie.values.length * barWidth;
         _.each(serie.values, function(d) {
             d.y0 = 0;
             d.y1 = d.y;
             // Start with -barwith/2 and + barWidth
-            d.w = (typeof(xStack[d.x]) === 'number' ? xStack[d.x] : (- stackSize/2 - barWidth)) + barWidth;
+            d.w = (typeof(xStack[d.x]) === 'number' ? xStack[d.x] : - barWidth*2) + barWidth;
             xStack[d.x] = d.w;
             console.log(d);
         });
